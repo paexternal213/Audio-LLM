7a086c0db2eda561718217ef3fd4afd276932829
paexternal213 <paexternal409@oaisb.info>
2025-02-25 22:56:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+#Libraries
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+#Object Initialization
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+#Rendering 'index.html' template
 @app.get("/",response_class=HTMLResponse)
 async def root(request:Request):
      return templates.TemplateResponse("index.html",{"request": request})
